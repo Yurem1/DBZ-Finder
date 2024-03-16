41adb8cacd089f5d981f516ecc2f13e2cca84c95
Yurem <yuremtovar1@gmail.com>
2024-03-15 23:09:54 -0500

    Add planet command to fetch and display information about a planet from the dragon ball universe

---
+++ commands/finder/planet.js
@@ -0,0 +1,55 @@
+const {
+  SlashCommandBuilder,
+  AttachmentBuilder,
+  EmbedBuilder
+} = require('discord.js');
+
+const { DRAGON_BALL, KRILLIN } = require('../../data/attachments/attachments.js');
+const { fetchPlanet } = require('../../data/api/fetch.js');
+const { displayError } = require('../../data/attachments/embeds.js');
+
+function displayPlanetEmbed(_obj) {
+  return new EmbedBuilder()
+    .setColor(0x00FF00)
+    .setTitle(_obj.name)
+    .setThumbnail('attachment://dragon_ball.png')
+    .setImage(_obj.image)
+    .setFooter({
+      text: `ID: ${_obj.id}`
+    })
+    .setTimestamp()
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('planet')
+    .setDescription('Learn more about a planet from the dragon ball universe!')
+    .addStringOption((option) => {
+      return option
+        .setName('name')
+        .setDescription('The planet you\'re searching for')
+        .setRequired(true);
+    }),
+  async execute(interaction) {
+    const value = interaction.options.getString('name');
+    const request = await fetchPlanet(value);
+
+    if (!!request[0] === false) {
+      const embed = displayError('Character not found, maybe you spelled it wrong?')
+
+      await interaction.reply({
+        embeds: [embed],
+        files: [KRILLIN]
+      });
+
+      return;
+    }
+
+    const embed = displayPlanetEmbed(request[0]);
+
+    await interaction.reply({
+      embeds: [embed],
+      files: [DRAGON_BALL]
+    });
+  }
+}
